4337bf82b64b16885468c96dc66080251c28a2f8
Charles Carter <charles.h.carter@gmail.com>
2016-01-25 20:42:28 -0800

    initialize all base files required for challenge

---
+++ spec/specs.js
@@ -0,0 +1,12 @@
+describe('pingPong', function() {
+  it("is a non integer value", function() {
+    expect(pingPong("foobar")).to.equal(false);
+  });
+
+  it("is an integer value", function() {
+    expect(pingPong(20)).to.
+
+  })
+
+
+  });
